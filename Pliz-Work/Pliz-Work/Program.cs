3ce2e2a154586975e519a6ab0ac75d4b322189ca
linne01lampa <linus.bensryd@gmail.com>
2018-10-18 09:43:20 +0200

    YEEET FOR Relz

--- Pliz-Work/Pliz-Work/Program.cs
+++ Pliz-Work/Pliz-Work/Program.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 using System.IO;
 
 
-namespace Pliz_Work
+namespace GridGame
 {
 
     class Program
@@ -17,51 +17,33 @@ namespace Pliz_Work
         static void Main(string[] args)
         {
 
-            Console.WriteLine("funkar");
-
-            //Game myGame = new Game(15, 6);
-            //Level level = new Level();
-            //RandomTestLevel rndLevel = new RandomTestLevel(10, 10);
-
-            // rndLevel.Draw(10, 10);
 
 
-            Game myGame = new Game(22, 8);
+            Game myGame = new Game(15, 6);
+            Level level = new Level();
+
             while (true)
             {
                 myGame.UpdateBoard();
                 myGame.DrawBoard();
-                //level.Start(0,0);
-                //Console.ReadKey();
-                //Console.ReadKey();
             }
+
         }
     }
 
     class Game
     {
+        int trys = 7;
 
         List<GameObject> GameObjects = new List<GameObject>();
-        List<LevelBase> levels = new List<LevelBase>();
-        List<Scene> scenes = new List<Scene>();
+
+        List<LevelBase> Levels = new List<LevelBase>();
+
+        List<Scene> Scenes = new List<Scene>();
 
 
         public Game(int xSize, int ySize)
         {
-
-
-            //levels.Add(new RandomTestLevel(10, 10));
-
-            //for (int i = 0; i < ySize + 2; i++)
-            //{
-            //    for (int j = 0; j < xSize + 2; j++)
-            //    {
-            //        if (j == 0 || i == 0 || i == ySize + 1 || j == xSize + 1)
-            //        {
-            //            GameObjects.Add(new Wall(j, i));
-            //        }
-            //    }
-            //}
 
             for (int i = 0; i < ySize + 2; i++)
             {
@@ -73,24 +55,33 @@ namespace Pliz_Work
                     }
                 }
             }
-            GameObjects.Add(new Player(5, 5));
+            GameObjects.Add(new Player(1, 1));
+            Levels.Add(new Level());
         }
 
         public void DrawBoard()
         {
+
+            if (trys == 7)
+            {
+                //yumymg
+
+
+                foreach (LevelBase i in Levels)
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    i.Start(0, 0);
+                }
+                trys = 0;
+            }
             foreach (GameObject gameObject in GameObjects)
             {
+                Console.ForegroundColor = ConsoleColor.Red;
                 gameObject.Draw(5, 3);
+                Console.ForegroundColor = ConsoleColor.White;
             }
 
-            foreach (LevelBase level in levels)
-            {
-
-            }
-
-
-
-
+            trys++;
 
         }
 
@@ -151,14 +142,10 @@ namespace Pliz_Work
 
         public override void Draw(int xBoxSize, int yBoxSize)
         {
-            int curX = XPosition * xBoxSize;
-            int curY = YPosition * yBoxSize;
+            int curX = XPosition;
+            int curY = YPosition;
             Console.SetCursorPosition(curX, curY);
-            Console.Write("█████");
-            Console.SetCursorPosition(curX, curY + 1);
-            Console.Write("█████");
-            Console.SetCursorPosition(curX, curY + 2);
-            Console.Write("█████");
+            Console.Write("█");
 
             lastY = curY;
             lastX = curX;
@@ -166,7 +153,8 @@ namespace Pliz_Work
 
         public override void Update()
         {
-            ConsoleKeyInfo keyInfo = Console.ReadKey();
+            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+            Console.CursorVisible = false;
 
             Erase();
 
@@ -191,91 +179,50 @@ namespace Pliz_Work
         public void Erase()
         {
             Console.SetCursorPosition(lastX, lastY);
-            Console.Write("     ");
-            Console.SetCursorPosition(lastX, lastY + 1);
-            Console.Write("     ");
-            Console.SetCursorPosition(lastX, lastY + 2);
-            Console.Write("     ");
+            Console.Write(" ");
         }
     }
 
-    abstract class Scene
+    abstract class LevelBase
     {
-        public abstract void Start();
+        public string fileText;
+
+        public abstract void Start(int x, int y);
     }
 
-    class MainMenu : Scene
+    class Level : LevelBase
     {
-        public override void Start()
-        {
-            Console.WriteLine("Main Menu");
-            Console.WriteLine("1: Start game");
-            Console.WriteLine("2: Load game");
-            Console.WriteLine("3: Highscore");
-            Console.WriteLine("4: Exit game");
-
-            bool rightAns = false;
-
-            while (!rightAns)
-            {
-                ConsoleKeyInfo keyInfo = Console.ReadKey();
-
-                if (keyInfo.Key == ConsoleKey.D1)
-                {
-                    rightAns = true;
-                }
-                else if (keyInfo.Key == ConsoleKey.D2)
-                {
-                    rightAns = true;
-                }
-                else if (keyInfo.Key == ConsoleKey.D3)
-                {
-                    rightAns = true;
-                }
-                else if (keyInfo.Key == ConsoleKey.D4)
-                {
-                    rightAns = true;
-                }
-                else
-                {
-                    rightAns = false;
-                }
-            }
-        }
-    }
-
-    class Level
-    {
+        string ranLevel = File.ReadAllText(Path.Combine(Environment.CurrentDirectory, "LevelOne.txt"));
+        char[] lines;
 
         Random rnd;
 
-        //string ranLevel = File.ReadAllText(@"C:\Users\patrik.fridh\Documents\Github\GridGame\Test.txt");
-        char[] lines;
         public Level()
         {
 
         }
 
-        public void Start(int startX, int startY)
+
+
+        public override void Start(int x, int y)
         {
-            int curY = startX;
-            int curX = startY;
-
-            rnd = new Random();
-
+            int curY = x;
+            int curX = y;
             int rndNum = 0;
-
             int offset = 1;
 
             bool write = false;
+            bool vertical = false;
+
+            rnd = new Random();
 
             List<Block> blocks = new List<Block>();
 
-            //lines = ranLevel.ToCharArray(0, ranLevel.Length);
+            lines = ranLevel.ToCharArray(0, ranLevel.Length);
 
             for (int i = 0; i < lines.Length; i++)
             {
-                rndNum = rnd.Next(3);
+                rndNum = rnd.Next(2);
 
                 char curChar = lines[i];
 
@@ -287,12 +234,14 @@ namespace Pliz_Work
                 if (curChar.ToString() == "0")
                 {
                     write = false;
+                    vertical = false;
                     curX += offset;
                 }
                 if (curChar.ToString() == "2")
                 {
                     write = false;
-                    curX = startX;
+                    vertical = false;
+                    curX = x;
                     curY += offset;
                 }
                 if (curChar.ToString() == "3")
@@ -306,11 +255,11 @@ namespace Pliz_Work
                         write = false;
                     }
 
+                    vertical = true;
                     curX += offset;
                 }
 
-
-                blocks.Add(new Block(curX, curY, write));
+                blocks.Add(new Block(curX, curY, write, vertical));
                 blocks[i].Draw(curX, curY);
             }
         }
@@ -323,11 +272,14 @@ namespace Pliz_Work
 
         bool write;
 
-        public Block(int xPos, int yPos, bool ifWrite)
+        bool vert;
+
+        public Block(int xPos, int yPos, bool ifWrite, bool vertical)
         {
             xPosition = xPos;
             yPosition = yPos;
             write = ifWrite;
+            vert = vertical;
         }
 
 
@@ -341,94 +293,122 @@ namespace Pliz_Work
 
             if (write)
             {
-                Console.SetCursorPosition(x, y);
-                Console.Write("█");
-                //Console.SetCursorPosition(x, y + 1);
-                //Console.Write("█");
+                if (vert)
+                {
+                    Console.SetCursorPosition(x, y);
+                    Console.Write("█");
+                    //Console.SetCursorPosition(x, y + 1);
+                    //Console.Write("██");
+
+                }
+                else if (!vert)
+                {
+                    Console.SetCursorPosition(x, y);
+                    Console.Write("█");
+                }
             }
             else
             {
-                Console.Write("    ");
+                Console.Write(" ");
             }
         }
     }
 
-    abstract class LevelBase
+    abstract class Scene
     {
-
-        public abstract void Draw(int x, int y);
-
+        public int index;
+        public abstract void Draw();
     }
 
-    class RandomTestLevel : LevelBase
+    class MainMenu : Scene
     {
-        int levelXSize;
-        int levelYSize;
-
-        bool RIGHT = true;
-        bool LEFT = true;
-        bool UP = true;
-        bool Down = true;
-
-        Random rnd = new Random();
-
-        int curX;
-        int curY;
-
-        bool done = false;
-
-        int moves = 0;
-
-        public RandomTestLevel(int xSize, int ySize)
+        public override void Draw()
         {
-            levelXSize = xSize;
-            levelYSize = ySize;
+            Console.WriteLine("MainMenu.");
+            Console.WriteLine();
+            Console.WriteLine("1: Play");
+            Console.WriteLine("2: Highscores");
+            Console.WriteLine("3: Load");
+            Console.WriteLine("4: Exit");
 
-            while (!done)
+            ConsoleKeyInfo keyInfo = Console.ReadKey();
+        }
+    }
+
+    static class HighScore
+    {
+        static string path = "HighScores.txt";
+        static List<int> HighScorePoints;
+        static List<string> HighScoreNames;
+
+        public static void ShowHighScores()
+        {
+            Console.WriteLine("Highscores.");
+            Console.WriteLine();
+
+            for (int i = 0; i < HighScorePoints.Count; i++)
             {
-                int dir = rnd.Next(4);
+                Console.WriteLine(HighScorePoints[i] + " " + HighScoreNames[i]);
+            }
+        }
 
-                //Console.WriteLine(dir);
+        public static void ReadHighScores()
+        {
+            HighScoreNames = new List<string>();
+            HighScorePoints = new List<int>();
+            string[] lines = System.IO.File.ReadAllLines(path);
 
-                if (dir == 0 && curX < 100)
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (i % 2 == 0)
                 {
-                    curX++;
-                }
-                if (dir == 1 && curX > 0 && curX < 100)
-                {
-                    curX--;
-                }
-                if (dir == 2 && curY < 100)
-                {
-                    curY++;
-                }
-                if (dir == 3 && curY > 0 && curX < 100)
-                {
-                    curY--;
+                    HighScorePoints.Add(Convert.ToInt32(lines[i]));
                 }
                 else
                 {
-                    curX++;
+                    HighScoreNames.Add(lines[i]);
                 }
+            }
+        }
 
-                Draw(curX, curY);
-
-                moves++;
-
-                if (moves == 100)
+        public static void WriteHighScores()
+        {
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(path))
+            {
+                for (int i = 0; i < HighScorePoints.Count; i++)
                 {
-                    done = true;
+                    file.WriteLine(HighScorePoints[i]);
+                    file.WriteLine(HighScoreNames[i]);
+                }
+            }
+        }
+
+        public static void AddHighScore(string name, int score)
+        {
+            for (int i = 0; i < HighScorePoints.Count; i++)
+            {
+                if (i == HighScorePoints.Count)
+                {
+                    HighScorePoints.Add(score);
+                    HighScoreNames.Add(name);
+                    break;
+                }
+                else if (score > HighScorePoints[i])
+                {
+                    HighScorePoints.Insert(i, score);
+                    HighScoreNames.Insert(i, name);
+                    break;
                 }
             }
 
-        }
-
-        public override void Draw(int x, int y)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write("████");
-            Console.SetCursorPosition(x, y + 1);
-            Console.Write("████");
+            if (HighScorePoints.Count > 5)
+            {
+                HighScorePoints.RemoveAt(6);
+                HighScoreNames.RemoveAt(6);
+            }
         }
     }
+
+
 }
+
